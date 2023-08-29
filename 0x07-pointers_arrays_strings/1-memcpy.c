bece28aa9f7cecb2715470f9fcd6b31689ae093f
shahd-03 <alshereifhager@gmail.com>
2023-08-29 14:49:27 -0700

    m

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_memcpy - function that copies memory area
+ *
+ * @dest : pointer to the destination array where the content is to be copied
+ * @src : pointer to the source of data to be copied
+ * @n : number of bytes to be copied
+ *
+ * Return: *dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+
+	return (dest);
+}
