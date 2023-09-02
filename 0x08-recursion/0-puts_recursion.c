be7627af48ff43170303efed4d33dc45f4536363
shahd-03 <alshereifhager@gmail.com>
2023-09-02 15:34:38 -0700

    m' git commit -m m git commit -m "m

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints a string, followed by a new line
+ *
+ * @s : this is a pointer
+ *
+ * Return : type voide so there is no return
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		putchar('\n');
+
+}
