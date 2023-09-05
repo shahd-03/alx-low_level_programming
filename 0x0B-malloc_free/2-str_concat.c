7283bbcc2cc1c5b43d1ce5c6f33b92c2a6904a89
shahd-03 <alshereifhager@gmail.com>
2023-09-05 11:00:14 -0700

    m

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -32,7 +32,7 @@ char *str_concat(char *s1, char *s2)
 	for (i = 0; s1[i] != '\0'; i++)
 		concat[i] = s1[i];
 	for (i = 0; s2[i] != '\0'; i++)
-		concat[len1] = s2[i];
+		concat[len1 + i] = s2[i];
 
 	return (concat);
 }
