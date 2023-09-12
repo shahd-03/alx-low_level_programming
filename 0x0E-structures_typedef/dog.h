4316b3a2c6d1dc7992d18026d441c88f05572679
shahd-03 <alshereifhager@gmail.com>
2023-09-11 17:10:06 -0700

    m

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+
+/**
+ * init_dog - Initializes a dog structure
+ * @d: A dog structure.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Return: Nothing
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
