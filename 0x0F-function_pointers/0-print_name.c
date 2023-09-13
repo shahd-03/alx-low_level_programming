2dd34688198e14d8da22cfa10d819b1070ebd598
shahd-03 <alshereifhager@gmail.com>
2023-09-13 01:17:27 -0700

    m

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include<stdlib.h>
+
+/**
+ * print_name - function that prints a name.
+ *
+ * @name : name of the person
+ * @f  : function pointer.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL)
+	{
+		f(name);
+	}
+}
