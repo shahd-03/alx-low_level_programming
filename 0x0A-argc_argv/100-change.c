f21aeb2c457d2ee8f037d0f32a9cca157f13a283
shahd-03 <alshereifhager@gmail.com>
2023-09-23 14:17:44 -0700

    m

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -  prints the minimum number of coins to
+ *  make change for an amount of money.
+ *
+ * @argc: arg count
+ * @argv: args
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int totl, cnt;
+	unsigned int i;
+	char *p;
+	int cents[] = {25, 10, 5, 2};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	totl = strtol(argv[1], &p, 10);
+	cnt = 0;
+
+	if (!*p)
+	{
+		while (total > 1)
+		{
+			for (i = 0; i < sizeof(cents[i]); i++)
+			{
+				if (totl >= cents[i])
+				{
+					cnt += totl / cents[i];
+					totl = totl % cents[i];
+				}
+			}
+		}
+		if (totl == 1)
+			cnt++;
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", cnt);
+	return (0);
+}
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - add a postive number
+ *
+ * @argc: arg count
+ * @argv: arg
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int i, s, sum = 0;
+	char *flag;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; argv[i]; i++)
+	{
+		n = strtol(argv[i], &flag, 10);
+		if (*flag)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			sum += s;
+		}
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
